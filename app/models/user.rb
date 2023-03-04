699f928462b20c957c43f15695ce778b840fe6b7
Satyajeet Desale <satyajeetndesale@gmail.com>
2023-03-04 19:14:56 +0530

    Online user tracking

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  def self.online
+    ids = ActionCable.server.pubsub.redis_connection_for_subscriptions.smembers("online_users")
+    where(id: ids)
+  end
+
+  def broadcast_online
+    broadcast_append_to "online_users", target: "online_users", partial: 'users/card', locals: { user: self }
+  end
+
+  def broadcast_offline
+    broadcast_remove_to "online_users", target: "user_#{id}"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_04_102413) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_04_130155) do
   create_table "favourite_technologies", force: :cascade do |t|
     t.integer "technology_id", null: false
     t.datetime "created_at", null: false
@@ -22,6 +22,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_04_102413) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "username", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "favourite_technologies", "technologies"
